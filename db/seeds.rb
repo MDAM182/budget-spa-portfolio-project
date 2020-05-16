9ddec5d7243236b6860ebb9a0268b248c18bd9f6
MDAM182 <mdam2345@gmail.com>
2020-05-16 18:42:33 -0400

    started backend, added user contrller, model, migration, seed data,

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create([
+  { username: "Alfa" , email: "alfa@code.com" , password_digest: "army1"},
+  { username: "Bravo" , email:"bravo@code.com" , password_digest: "army1"},
+  { username: "Delta" , email:"delta@code.com" , password_digest: "army1"},
+  { username: "Whiskey" , email:"whiskey@code.com" , password_digest: "army1"},
+  { username: "Echo" , email:"echo@code.com", password_digest: "army1"},
+  { username: "Tango" , email:"tango@code.com", password_digest: "army1"},
+
+  ])
